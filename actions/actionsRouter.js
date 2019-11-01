51f1e8c28077172a37a34a579f973768efdafb0b
Oluwajoba <oluwajobabello@gmail.com>
2019-11-01 10:46:36 +0100

    create new folder / actions / add file actionsRouter.js and import actionDb / add action url to router and use in server / server.js

---
+++ actions/actionsRouter.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const actionDb = require("../data/helpers/actionModel");
+
+const router = express.Router();
+
+
+
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const server = express();
 const router = require("./projects/projectsRouter");
+const router2 = require("./actions/actionsRouter");
+
 
 server.use(express.json());
 server.use("/api/projects", router);
+server.use("/api/actions", router2);
 
 server.get("/", (req, res) => {
     res.send("hello world");
