0f66b9fd4a307e6d83a2c7df674aa7b3aee77a7f
Oluwajoba <oluwajobabello@gmail.com>
2019-11-01 10:48:55 +0100

    build router.get / get() / actionsRouter.js

--- actions/actionsRouter.js
+++ actions/actionsRouter.js
@@ -4,7 +4,15 @@ const actionDb = require("../data/helpers/actionModel");
 
 const router = express.Router();
 
-
+router.get('/', (req, res) => {
+    actionDb.get()
+    .then((actions) => {
+        res.status(200).json(actions)
+    })
+    .catch(() => {
+        res.status(500).json({ error: "The actions information could not be retrieved." })
+    })
+});
 
 
 
