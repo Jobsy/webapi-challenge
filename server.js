1be946f4c56119b698aa050bfd6a7936f11c45d7
Oluwajoba <oluwajobabello@gmail.com>
2019-11-01 09:26:41 +0100

    inport router from projectsRouter and use with server / server.js

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const server = express();
+const router = require("./project/projectRouter");
 
 server.use(express.json());
+server.use("/api/projects", router);
 
 server.get("/", (req, res) => {
     res.send("hello world");
