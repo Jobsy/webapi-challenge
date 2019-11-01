32d9c9551b633620951bc74ce7bd9cc341cbbbe9
Oluwajoba <oluwajobabello@gmail.com>
2019-11-01 11:18:06 +0100

    import  cors / server.js / project set-up / stretch

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const server = express();
+const cors = require("cors");
 const router = require("./projects/projectsRouter");
 const router2 = require("./actions/actionsRouter");
 
 
 server.use(express.json());
+server.use(cors());
 server.use("/api/projects", router);
 server.use("/api/actions", router2);
 
